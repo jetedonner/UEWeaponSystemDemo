670bd0b3c4d48b3f9576966d70f7fab24514341c
Jetedonner <kim@kimhauser.ch>
2022-08-27 14:24:18 +0200

    Some cleanup

--- Source/UEWeaponSystemDemo/UEWeaponSystemDemoCharacter.h
+++ Source/UEWeaponSystemDemo/UEWeaponSystemDemoCharacter.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c65b9732976c76256e5b1c6ff64191eccbd79db00432f02f3c3a64429fb883f9
-size 2508
+oid sha256:275cf7f31625a6c48af5a2ea4121a413539517d81d593bd1d70a503d44c62380
+size 2515
